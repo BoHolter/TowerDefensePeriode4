984b460e26c9c95110126904cf0c44ed20ee384b
Luuk17 <2194914@talnet.nl>
2024-05-29 10:42:29 +0200

    better

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+
     [SerializeField]
     private GameObject Enemy1Prefab;
-    private float Enemy1Interval = 3.5f;
- 
+    private float Enemy1Interval = 0.5f;
+    [SerializeField]
+    private GameObject Enemy2Prefab;
+    private float Enemy2Interval = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(spawnEnemy(Enemy1Interval, Enemy1Prefab));
+        StartCoroutine(spawnEnemy(Enemy2Interval, Enemy2Prefab));
     }
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     { 
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        Vector3 pos = transform.position;
+        GameObject newEnemy = Instantiate(enemy, pos , Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
